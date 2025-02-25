d1d97d54b9c6466225b5020b68336ea29e26e39a
ArtemRomaniuk <romaniuk.artem@student.uzhnu.edu.ua>
2025-02-25 23:29:07 +0200

    modified Stack.js

--- practice/Stack.js
+++ practice/Stack.js
@@ -7,27 +7,47 @@ class Stack {
     // unshift - додає перший
     // shift - видаляє перший
 
-    addToStack(element) {
-        return this.items.push(element);
+    add(element) {
+        this.items.push(element);
     }
 
-    removeFromStack() {
+    remove() {
         if (this.items.length === 0) {
             console.log("Цей стек пустий")
+        } else {
+            return this.items.pop();
         }
-        return this.items.pop();
     }
 
-    checkStack() {
-        return this.items[this.items.length - 1];
-    }
-
-    clearStack() {
+    clear() {
         this.items = [];
     }
 
-    searchInStack(element) {
-        return this.items.includes(element);
+    peekLast() {
+        return this.items[this.items.length - 1];
+    }
+
+    peek(index) {
+        return this.items[index];
+    }
+
+    indexOf(element) {
+        let tempArr = [];
+        for (let i = this.items.length - 1; i >= 0; i--) {
+            if (this.items[i] !== element) {
+                tempArr.push(this.items[i]);
+                this.remove();
+            } else {
+                for (let j = tempArr.length; j >= 0; j--) {
+                    if (tempArr[j] !== undefined) this.add(tempArr[j]);
+                }
+                return i;
+            }
+        }
+        for (let j = tempArr.length; j >= 0; j--) {
+            if (tempArr[j] !== undefined) this.add(tempArr[j]);
+        }
+        return -1;
     }
 
     testShow() {
@@ -36,7 +56,17 @@ class Stack {
 }
 
 let stack = new Stack();
-stack.addToStack(5);
-stack.addToStack(15);
+stack.add(5);
+stack.add(15);
+stack.add(25)
+stack.add(35)
+stack.add(45)
 stack.testShow();
-console.log(stack.searchInStack(15));
+console.log('Індекс 25: ' + stack.indexOf(25));
+stack.testShow();
+console.log(stack.peekLast());
+console.log(stack.peek(1));
+stack.remove()
+stack.testShow()
+stack.clear();
+stack.testShow()
