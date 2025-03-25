818c852a0e2a10a822952e1af96911c61ddb1cb9
ArtemRomaniuk <romaniuk.artem@student.uzhnu.edu.ua>
2025-03-25 19:59:22 +0200

    fixed lab_4

--- labs/lab_4/task1.1.js
+++ labs/lab_4/task1.1.js
@@ -3,12 +3,18 @@ function y(a, b, x) {
 
     if (x < 0) return "Функція не визначена";
 
-    if (x >= 0 && x < 9) return (Math.cos(x) * 3 * x * x + b * x * Math.log((a * x + b)));
+    if (x >= 0 && x < 4) return Math.cos(x);
+    if (x === 4) return (3 * x * x + b * x);
+    if (x > 4 && x < 9) return Math.log(a * x + b);
 
     if (x >= 9) return "Функція не визначена";
 }
 
 console.log(y(2, 2, -3));
 console.log(y(1, 2, -1));
-console.log(y(1, 1, 1));
+
+console.log(y(1, 1, 0));
+console.log(y(1, 1, 4));
+console.log(y(1, 1, 5));
+
 console.log(y(1, 1, 9));
--- labs/lab_4/task1.2.js
+++ labs/lab_4/task1.2.js
@@ -1,6 +1,11 @@
 function y(a, b, x) {
     if (x >= 0) {
-        if (x < 9) return (Math.cos(x) * 3 * x * x + b * x * Math.log((a * x + b)));
+        if (x < 4) return Math.cos(x);
+        else if (x === 4) return (3 * x * x + b * x);
+        else if (x > 4) {
+            if (x < 9) return Math.log(a * x + b);
+            else return "Функція не визначена";
+        }
         else return "Функція не визначена";
     }
     else {
@@ -18,5 +23,9 @@ function y(a, b, x) {
 
 console.log(y(2, 2, -3));
 console.log(y(1, 2, -1));
-console.log(y(1, 1, 1));
+
+console.log(y(1, 1, 0));
+console.log(y(1, 1, 4));
+console.log(y(1, 1, 5));
+
 console.log(y(1, 1, 9));
