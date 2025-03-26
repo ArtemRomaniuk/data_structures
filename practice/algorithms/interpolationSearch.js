1bf78a6d3f7cc4945acd2d080d9691f99775b20f
ArtemRomaniuk <romaniuk.artem@student.uzhnu.edu.ua>
2025-03-26 08:58:21 +0200

    updated interpolationSort.js

--- practice/algorithms/interpolationSearch.js
+++ practice/algorithms/interpolationSearch.js
@@ -2,8 +2,8 @@ function interpolationSearch(sortedArr, element) {
     let low = 0;
     let high = sortedArr.length - 1;
 
-    while (sortedArr[low] <= element && element <= sortedArr[high]) {
-        let mid = low + ((element - sortedArr[low]) * (high - low) / (sortedArr[high] - sortedArr[low]));
+    while (low < high && sortedArr[low] <= element && element <= sortedArr[high]) {
+        let mid = low + Math.floor((element - sortedArr[low]) * (high - low) / (sortedArr[high] - sortedArr[low]));
 
         if (element === sortedArr[mid]) return mid;
         if (element > sortedArr[mid]) low = mid + 1;
@@ -12,5 +12,5 @@ function interpolationSearch(sortedArr, element) {
     return -1;
 }
 
-console.log(interpolationSearch([10, 20, 30, 40, 50], 10));
+console.log(interpolationSearch([10, 22, 35, 40, 50], 35));
 console.log(interpolationSearch([10, 20, 30, 40, 50], -30));
