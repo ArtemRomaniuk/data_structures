8377389bbf05f279667325fe23278ffadbe2af32
ArtemRomaniuk <romaniuk.artem@student.uzhnu.edu.ua>
2025-06-02 23:12:54 +0300

    edited priority queue

--- practice/structures/PriorityQueue.js
+++ practice/structures/PriorityQueue.js
@@ -3,7 +3,6 @@ class PriorityQueue {
         this.queue = [];
     }
 
-    // Додавання елемента в чергу з пріоритетом
     add(element, priority) {
         const queueElement = { element, priority };
         let added = false;
@@ -21,7 +20,6 @@ class PriorityQueue {
         }
     }
 
-    // Видалення та повернення елемента з найвищим пріоритетом
     remove() {
         return this.queue.shift()?.element;
     }
@@ -30,23 +28,20 @@ class PriorityQueue {
         this.queue = [];
     }
 
-    // Отримання елемента з найвищим пріоритетом без видалення
     peek() {
         return this.queue[0]?.element;
     }
 
-    // Виведення черги у вигляді рядка
     printQueue() {
-        return this.queue.map(q => `(${q.element}, ${q.priority})`).join(' -> ');
+        console.log(this.queue.map(q => `(${q.element}, ${q.priority})`).join(' -> '));
     }
 }
 
-// Приклад використання
 const pq = new PriorityQueue();
 pq.add("A", 2);
 pq.add("B", 1);
 pq.add("C", 3);
 
-console.log(pq.printQueue()); // (B, 1) -> (A, 2) -> (C, 3)
-console.log(pq.remove()); // B
-console.log(pq.peek()); // A
+pq.printQueue();
+console.log(pq.remove());
+console.log(pq.peek());
